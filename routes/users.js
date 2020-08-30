b0754fe78f2a669f3c5d7e675b8fe12c75856a16
Emmanuel Adebayo <eadebayo15@gmail.com>
2020-08-31 00:38:39 +0100

    Modified

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const multer = require('multer');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
